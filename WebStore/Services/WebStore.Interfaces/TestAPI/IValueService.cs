933b678140ae123fe0fe51fed8dac86e8f8cda50
Kusn <serjikf@yandex.ru>
2021-10-13 13:57:04 +0300

    Интерфейс сервиса тестового WebAPI

---
+++ WebStore/Services/WebStore.Interfaces/TestAPI/IValueService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Interfaces.TestAPI
+{
+    public interface IValueService
+    {
+        IEnumerable<string> GetAll();
+
+        int Count();
+
+        string GetById(int id);
+
+        void Add(string value);
+
+        void Edit(int id, string value);
+
+        bool Delete(int id);
+    }
+}
