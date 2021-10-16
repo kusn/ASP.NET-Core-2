8e273a0f593edfd2f25cfc70eb79b20f445620a0
Kusn <serjikf@yandex.ru>
2021-10-16 13:01:46 +0300

    Клиент WebAPI управления сотрудниками

---
+++ WebStore/Services/WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Models;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(HttpClient client) : base(client, "api/employees")
+        {
+
+        }
+
+        public int Add(Employee employee)
+        {
+            var response = Post(Address, employee);
+            var added_employee = response.Content.ReadFromJsonAsync<Employee>();
+            var id = added_employee.Id;
+            return id;
+        }
+
+        public bool Delete(int id)
+        {
+            var response = Delete($"{Address}/{id}");
+            var success = response.IsSuccessStatusCode;
+            return success;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            var employees = Get<IEnumerable<Employee>>(Address);
+            return employees;
+        }
+
+        public Employee GetById(int id)
+        {
+            var result = Get<Employee>($"{Address}/{id}");
+            return result;
+        }
+
+        public void Update(Employee employee)
+        {
+            Put(Address, employee);
+        }
+    }
+}
