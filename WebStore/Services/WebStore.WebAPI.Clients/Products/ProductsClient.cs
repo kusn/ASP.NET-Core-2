588b4cc3fec071fc2399b97b4fb4c9fafd18a297
Kusn <serjikf@yandex.ru>
2021-10-17 10:48:53 +0300

    WebAPI клиент для каталога товаров

---
+++ WebStore/Services/WebStore.WebAPI.Clients/Products/ProductsClient.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using WebStore.Domain;
+using WebStore.Domain.DTO;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Products
+{
+    public class ProductsClient : BaseClient, IProductData
+    {
+        public ProductsClient(HttpClient client) : base(client, "api/products")
+        {
+
+        }
+
+        public Brand GetBrandById(int id)
+        {
+            var brand = Get<BrandDTO>($"{Address}/brands/{id}");
+            return brand.FromDTO();
+        }
+
+        public IEnumerable<Brand> GetBrands()
+        {
+            var brands = Get<IEnumerable<BrandDTO>>($"{Address}/brands");
+            return brands.FromDTO();
+        }
+
+        public Product GetProductById(int id)
+        {
+            var product = Get<ProductDTO>($"{Address}/{id}");
+            return product.FromDTO();
+        }
+
+        public IEnumerable<Product> GetProducts(ProductFilter filter = null)
+        {
+            var response = Post(Address, filter ?? new());
+            var products_dtos = response.Content.ReadFromJsonAsync<IEnumerable<ProductDTO>>().Result;
+            return products_dtos.FromDTO();
+        }
+
+        public Section GetSectionById(int id)
+        {
+            var section = Get<SectionDTO>($"{Address}/sections/{id}");
+            return section.FromDTO();
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            var sections = Get<IEnumerable<SectionDTO>>($"{Address}/sections");
+            return sections.FromDTO();
+        }
+    }
+}
