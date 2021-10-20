2e86a4463ced8e1bc4ee5cf9243d6315c9a5cd8d
kusn <serjikf@yandex.ru>
2021-10-20 12:50:26 +0300

    Заготовка роли контроллеров IDentity

--- WebStore/Services/WebStore.WebAPI/Controllers/Identity/RoleApiController.cs
+++ WebStore/Services/WebStore.WebAPI/Controllers/Identity/RoleApiController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces;
 
 namespace WebStore.WebAPI.Controllers.Identity
@@ -7,5 +13,17 @@ namespace WebStore.WebAPI.Controllers.Identity
     [Route(WebAPIAdresses.Identity.Roles)]
     public class RoleApiController : ControllerBase
     {
+        private readonly RoleStore<Role> _RoleStore;
+
+        public RoleApiController(WebStoreDB db)
+        {
+            _RoleStore = new (db);
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<Role>> GetAll()
+        {
+            return await _RoleStore.Roles.ToArrayAsync();
+        }
     }
 }
