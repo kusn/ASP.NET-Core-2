e3dd181395b16de912e01e434807430f06642580
kusn <serjikf@yandex.ru>
2021-10-22 19:15:38 +0300

    Метод расширения регистрации провайдера логов

--- WebStore/Common/WebStore.Logger/Log4NetLogger.cs
+++ WebStore/Common/WebStore.Logger/Log4NetLogger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +7,24 @@ using System.Threading.Tasks;
 
 namespace WebStore.Logger
 {
-    public static class Log4NetLoggerFactoryExtesions
+    public class Log4NetLoggerProvider : ILoggerProvider
     {
+        public Log4NetLoggerProvider(string configurationFile)
+        {
 
+        }
+        
+        public ILogger CreateLogger(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    public class Log4NetLoggerProvider
-    {
-
-    }
-    
     public class Log4NetLogger
     {
         
---
+++ WebStore/Common/WebStore.Logger/Log4NetLoggerFactoryExtesions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetLoggerFactoryExtesions
+    {
+        private static string CheckFilePath(string filePath)
+        {
+            //if (filePath is null || filePath.Length == 0)
+            if (filePath is not { Length: > 0 })
+                throw new ArgumentException("Не указан путь к файлу");
+
+            if (Path.IsPathRooted(filePath))
+                return filePath;
+
+            var assembly = Assembly.GetEntryAssembly();
+            var dir = Path.GetDirectoryName(assembly!.Location);
+            return Path.Combine(dir!, filePath);
+        }
+
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory factory, string configurationFile = "log4net.config")
+        {
+            factory.AddProvider(new Log4NetLoggerProvider(CheckFilePath(configurationFile)));
+            return factory;
+        }
+    }
+}
