3b0463113cb5fc96315f6897d0eb4467c867d0c7
kusn <serjikf@yandex.ru>
2021-10-22 19:27:26 +0300

    Провайдер

--- WebStore/Common/WebStore.Logger/Log4NetLogger.cs
+++ WebStore/Common/WebStore.Logger/Log4NetLogger.cs
@@ -1,29 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WebStore.Logger
 {
-    public class Log4NetLoggerProvider : ILoggerProvider
-    {
-        public Log4NetLoggerProvider(string configurationFile)
-        {
-
-        }
-        
-        public ILogger CreateLogger(string categoryName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-    }
 
     public class Log4NetLogger
     {
---
+++ WebStore/Common/WebStore.Logger/Log4NetLoggerProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+using System.Xml;
+
+namespace WebStore.Logger
+{
+    public class Log4NetLoggerProvider : ILoggerProvider
+    {
+        private readonly string _СonfigurationFile;
+        private readonly ConcurrentDictionary<string, Log4NetLogger> _Loggers = new();
+
+        public Log4NetLoggerProvider(string configurationFile)
+        {
+            _СonfigurationFile = configurationFile;
+        }
+
+        public ILogger CreateLogger(string category) =>
+            _Loggers.GetOrAdd(category, category =>
+            {
+                var xml = new XmlDocument();
+                xml.Load(_СonfigurationFile);
+                return new Log4NetLogger(category, xml["log4net"]);
+            });
+
+        public void Dispose()
+        {
+            _Loggers.Clear();
+        }
+    }
+}
