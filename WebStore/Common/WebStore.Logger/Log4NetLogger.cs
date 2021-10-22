080ed15a16c02a5b8ebed0470948d0df7dd43396
kusn <serjikf@yandex.ru>
2021-10-22 18:53:20 +0300

    Файлы конфигурации библиотеки Log4NET

---
+++ WebStore/Common/WebStore.Logger/Log4NetLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetLoggerFactoryExtesions
+    {
+
+    }
+
+    public class Log4NetLoggerProvider
+    {
+
+    }
+    
+    public class Log4NetLogger
+    {
+        
+    }
+}
