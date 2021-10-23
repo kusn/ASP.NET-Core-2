5b2067b489b49d7c202124f800507a18ce36ceac
kusn <serjikf@yandex.ru>
2021-10-23 16:52:19 +0300

    Serilog. Конфигурация

--- WebStore/UI/WebStore/Program.cs
+++ WebStore/UI/WebStore/Program.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using System;
 using System.Threading.Tasks;
 
 namespace WebStore
@@ -18,17 +22,26 @@ namespace WebStore
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(host =>host
+                .ConfigureWebHostDefaults(host => host
                     .UseStartup<Startup>()
-                    //.ConfigureLogging((host, log) => log
-                    //    .ClearProviders()
-                    //    .AddDebug()
-                    //    .AddConsole(c => c.IncludeScopes = true)
-                    //    .AddEventLog()
-                    //    .AddFilter("Microsoft", LogLevel.Information)
-                    //    .AddFilter<ConsoleLoggerProvider>("Microsoft.EntityFrameworkCore", LogLevel.Warning)
-                    //)
+                //.ConfigureLogging((host, log) => log
+                //    .ClearProviders()
+                //    .AddDebug()
+                //    .AddConsole(c => c.IncludeScopes = true)
+                //    .AddEventLog()
+                //    .AddFilter("Microsoft", LogLevel.Information)
+                //    .AddFilter<ConsoleLoggerProvider>("Microsoft.EntityFrameworkCore", LogLevel.Warning)
+                //)
                 )
+            .UseSerilog((host, log) => log.ReadFrom.Configuration(host.Configuration)
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Console(
+                outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level:u3}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+            .WriteTo.RollingFile($@".\Logs\WebStore.WebAPI[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log")
+            .WriteTo.File(new JsonFormatter(",", true), $@".\Logs\WebStore.WebAPI[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log.json")
+            )
             ;
     }
 }
