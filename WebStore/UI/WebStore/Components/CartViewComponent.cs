3a071c7b86aaf6b70b6f4ca6a9dfa3a38a363a84
Kusn <serjikf@yandex.ru>
2021-11-02 19:16:06 +0300

    Подготовка логики для внедрения скрипта

---
+++ WebStore/UI/WebStore/Components/CartViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartService _CartService;
+
+        public CartViewComponent(ICartService cartService)
+        {
+            _CartService = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.Count = _CartService.GetViewModel().ItemsCount;
+
+            return View();
+        }
+    }
+}
