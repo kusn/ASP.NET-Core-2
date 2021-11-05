921ecd9ae3835eacf767dc9693c76f6d1bcc6261
Kusn <serjikf@yandex.ru>
2021-11-05 15:56:32 +0300

    Отключение тестового кода

--- WebStore/WebStore.TestConsole/Program.cs
+++ WebStore/WebStore.TestConsole/Program.cs
@@ -9,18 +9,18 @@ namespace WebStore.TestConsole
     {        
         static async Task Main(string[] args)
         {
-            var client = new HttpClient()
-            {
-                //BaseAddress = new Uri("http://localhost:5001")
-            };
+            //var client = new HttpClient()
+            //{
+            //    //BaseAddress = new Uri("http://localhost:5001")
+            //};
 
-            var api = new WebAPIClient("http://localhost:5001", client);
+            //var api = new WebAPIClient("http://localhost:5001", client);
 
-            var products = api.ProductsAsync(new ProductFilter());
+            //var products = api.ProductsAsync(new ProductFilter());
 
-            var employee = await api.Employees4Async(2);
+            //var employee = await api.Employees4Async(2);
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
