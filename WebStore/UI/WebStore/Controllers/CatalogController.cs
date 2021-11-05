b0695e54ad020e142a1c72dc0e918bb4ec859e2a
Kusn <serjikf@yandex.ru>
2021-11-05 15:56:11 +0300

    Формирование html-фрагмента для скрипта

--- WebStore/UI/WebStore/Controllers/CatalogController.cs
+++ WebStore/UI/WebStore/Controllers/CatalogController.cs
@@ -5,11 +5,14 @@ using WebStore.Interfaces.Services;
 using WebStore.Domain.ViewModels;
 using WebStore.Services.Mapping;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 
 namespace WebStore.Controllers
 {
     public class CatalogController : Controller
     {
+        private const string __PageSizeConfig = "CatalogPageSize";
+
         private readonly IProductData _ProductData;
         private readonly IConfiguration _Configuration;
 
@@ -22,7 +25,7 @@ namespace WebStore.Controllers
         public IActionResult Index(int? BrandId, int? SectionId, int page = 1, int? pageSize = null)
         {
             var page_size = pageSize
-                ?? (int.TryParse(_Configuration["CatalogPageSize"], out var value) ? value: null);
+                ?? (int.TryParse(_Configuration[__PageSizeConfig], out var value) ? value: null);
             
             var filter = new ProductFilter
             {
@@ -58,6 +61,26 @@ namespace WebStore.Controllers
                 return NotFound();
 
             return View(product.ToView());
-        }        
+        }
+
+        public IActionResult GetProductsView(int? BrandId, int? SectionId, int page = 1, int? pageSize = null)
+        {
+            var products = GetProducts(BrandId, SectionId, page, pageSize);
+
+            return PartialView("Partial/_Products", products);
+        }
+
+        public IEnumerable<ProductViewModel> GetProducts(int? BrandId, int? SectionId, int page, int? pageSize)
+        {
+            var products = _ProductData.GetProducts(new()
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+                Page = page,
+                PageSize = pageSize ?? _Configuration.GetValue(__PageSizeConfig, 6),
+            });
+
+            return products.Products.OrderBy(p => p.Order).ToView();
+        }
     }
 }
